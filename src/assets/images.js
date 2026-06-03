888b01c019bd5477371664662755d888ac98d2a4
Atharva Bide <atharva@oscprofessionals.in>
2026-06-03 12:45:43 +0530

    fixed images path issue

--- src/assets/images.js
+++ src/assets/images.js
@@ -4,27 +4,27 @@
 
 // Brand / hero
 export { default as oscWhiteLogo } from './img/OSC-White-logo.png';
-export { default as homepageBanner } from './img/homepage-banner.webp';
+export { default as homepageBanner } from './img/homepage-banner.png';
 
 // Marketplace app logos
-export { default as wholesaleB2bLogo } from './img/oscp-wholesale-b2b-app-logo.webp';
-export { default as pricingDiscountSuiteLogo } from './img/oscp-pricing-and-discount-suite-app-logo.webp';
-export { default as bundleUpsellLogo } from './img/oscp-bundle-and-upsell-app-logo.webp';
-export { default as shippingDiscountLogo } from './img/oscp-shipping-discount-app-logo.webp';
-export { default as hideShippingPaymentLogo } from './img/oscp-hide-shipping-and-payment-app-logo.webp';
-export { default as swatchesLogo } from './img/oscp-swatches-app-logo.webp';
-export { default as addOnsTierPricingLogo } from './img/oscp-add-ons-tier-pricing-app-logo.webp';
+export { default as wholesaleB2bLogo } from './img/oscp-wholesale-b2b-app-logo.png';
+export { default as pricingDiscountSuiteLogo } from './img/oscp-pricing-and-discount-suite-app-logo.png';
+export { default as bundleUpsellLogo } from './img/oscp-bundle-and-upsell-app-logo.png';
+export { default as shippingDiscountLogo } from './img/oscp-shipping-discount-app-logo.png';
+export { default as hideShippingPaymentLogo } from './img/oscp-hide-shipping-and-payment-app-logo.png';
+export { default as swatchesLogo } from './img/oscp-swatches-app-logo.png';
+export { default as addOnsTierPricingLogo } from './img/oscp-add-ons-tier-pricing-app-logo.png';
 
 // "We work with" segment images
-export { default as weWorkWithRetail } from './img/we-work-with-retail.webp';
-export { default as weWorkWithAgencies } from './img/we-work-with-agencies.webp';
-export { default as weWorkWithB2b } from './img/we-work-with-b2b.webp';
+export { default as weWorkWithRetail } from './img/we-work-with-retail.png';
+export { default as weWorkWithAgencies } from './img/we-work-with-agencies.png';
+export { default as weWorkWithB2b } from './img/we-work-with-b2b.png';
 
 // Strength stats
-export { default as strengthHappyCustomers } from './img/strength-happy-customers.webp';
-export { default as strengthEstablished } from './img/strength-established.webp';
-export { default as strengthDevelopers } from './img/strength-developers.webp';
-export { default as strengthProjects } from './img/strength-projects.webp';
+export { default as strengthHappyCustomers } from './img/strength-happy-customers.png';
+export { default as strengthEstablished } from './img/strength-established.png';
+export { default as strengthDevelopers } from './img/strength-developers.png';
+export { default as strengthProjects } from './img/strength-projects.png';
 
 // B2B page feature screenshots
 export { default as featTierPricing } from './img/features/Tier-Pricing-B2B-Wholesale-Pricing-1.png';
@@ -34,11 +34,11 @@ export { default as featQuickOrderForm } from './img/features/Quick-Order-Form-1
 export { default as featTaxDisplay } from './img/features/Tax-display-1200x774-1.png';
 
 // Portfolio
-export { default as portfolioRockher } from './img/portfolio-rockher.webp';
-export { default as portfolioVoylite } from './img/portfolio-voylite.webp';
-export { default as portfolioNewwall } from './img/portfolio-newwall.webp';
-export { default as portfolioVitalife } from './img/portfolio-vitalife.webp';
-export { default as portfolioVitaliving } from './img/portfolio-vitaliving.webp';
-export { default as portfolioMyBongShop } from './img/portfolio-my-bong-shop.webp';
-export { default as portfolioDrivenByStyle } from './img/portfolio-driven-by-style.webp';
-export { default as portfolioRxlabels } from './img/portfolio-rxlabels.webp';
+export { default as portfolioRockher } from './img/portfolio-rockher.png';
+export { default as portfolioVoylite } from './img/portfolio-voylite.png';
+export { default as portfolioNewwall } from './img/portfolio-newwall.png';
+export { default as portfolioVitalife } from './img/portfolio-vitalife.png';
+export { default as portfolioVitaliving } from './img/portfolio-vitaliving.png';
+export { default as portfolioMyBongShop } from './img/portfolio-my-bong-shop.png';
+export { default as portfolioDrivenByStyle } from './img/portfolio-driven-by-style.png';
+export { default as portfolioRxlabels } from './img/portfolio-rxlabels.png';
